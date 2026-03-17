63ebb311608d19633118aa47db68047bbbf6ad2e
Sam Vervaeck <samvv@pm.me>
2026-03-17 20:50:57 +0100

    Export more from index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 
 export { type Transport, WebSocketTransport, DummyTransport, RawTransport, createDuplex,  } from "./transport.js"
-export { implement, contract } from "./types.js"
-export { RPC, type AnyMethods, type AnyEvents } from "./rpc.js"
+export { implement, contract, emptyContract, anyContract } from "./types.js"
+export { RPC, type AnyMethods, type AnyEvents, connect } from "./rpc.js"
 export { createProxy } from "./proxy.js"
 

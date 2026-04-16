da830a3cbb798ed318fc7303ddd378eb1881caf5
Sam Vervaeck <samvv@pm.me>
2026-04-16 17:37:55 +0200

    Improve isolation of tests a bit

--- src/integration.test.ts
+++ src/integration.test.ts
@@ -2,7 +2,7 @@ import { test, expect } from "bun:test"
 import t from "reflect-types";
 import stream from "node:stream"
 
-import { contract, implement } from "./types.js"
+import { contract, emptyContract, implement } from "./types.js"
 import { connect } from "./rpc.js"
 import { FailedValidationError } from "./error.js";
 import { sleep } from "bun";
@@ -13,7 +13,6 @@ const leftContract = contract({
   methods: {
     getState: t.callable([] as const, t.number()),
     setState: t.callable([ t.number() ] as const, t.undefined()),
-    returnInvalid: t.callable([] as const, t.number()),
   },
   events: {
     someevent: t.string(),
@@ -25,7 +24,6 @@ const rightContract = contract({
     getState: t.callable([] as const, t.number()),
     setState: t.callable([ t.number() ] as const, t.undefined()),
     getLength: t.callable([ t.string() ] as const, t.number()),
-    slow11: t.callable([] as const, t.object({ slow: t.promise(t.number()) })),
   }
 });
 
@@ -64,12 +62,6 @@ const leftImpl = implement(leftContract, rightContract)
       state.foo = newFoo;
     },
   })
-  .methods({
-    // @ts-expect-error Wrong function return type on purpose
-    returnInvalid() {
-      return "a string";
-    }
-  })
   .finish();
 
 const rightImpl = implement(rightContract, leftContract)
@@ -77,9 +69,6 @@ const rightImpl = implement(rightContract, leftContract)
   .methods({
     getState({ state }) {
       return state.foo;
-    },
-    slow11: () => {
-      return { slow: (async () => { await sleep(250); return 11; })() };
     },
     setState({ state}, newState) {
       state.foo = newState;
@@ -89,6 +78,44 @@ const rightImpl = implement(rightContract, leftContract)
     }
   })
   .finish();
+
+test('can call methods on the associated client', done => {
+
+  const leftContract = contract({
+    methods: {
+      pong: t.callable([ t.any() ] as const, t.void()),
+    }
+  });
+
+  const rightContract = contract({
+    methods: {
+      ping: t.callable([ t.any() ] as const, t.void()),
+    }
+  });
+
+  const PASSED_IN = 42;
+
+  const leftImpl = implement(leftContract, rightContract)
+    // .methods(stateHandlers())
+    .method('pong', (_, x) => {
+        expect(x).toStrictEqual(PASSED_IN);
+        done();
+    })
+    .finish();
+
+  const rightImpl = implement(rightContract, leftContract)
+    .method('ping', (req, x) => {
+      req.client.pong(x);
+    })
+    .finish()
+
+  const [leftTransport, rightTransport] = createDuplex();
+  const leftServer = connect(leftImpl, leftTransport, {});
+  const rightServer = connect(rightImpl, rightTransport, {});
+
+  leftServer.ping(PASSED_IN);
+
+});
 
 
 test('can call methods on both sides', async () => {
@@ -126,6 +153,25 @@ test('throws an error on invalid param count', async () => {
 });
 
 test('throws an error on invalid return', async () => {
+
+  const leftContract = contract({
+    methods: {
+      returnInvalid: t.callable([] as const, t.number()),
+    }
+  });
+
+  const rightContract = emptyContract();
+
+  const leftImpl = implement(leftContract, rightContract)
+    // @ts-expect-error Wrong function return type on purpose
+    .method('returnInvalid', () => {
+      return "a string";
+    })
+    .finish();
+
+  const rightImpl = implement(rightContract, leftContract)
+    .finish();
+
   const [leftTransport, rightTransport] = createDuplex();
   const left = connect(leftImpl, leftTransport, { foo: 42 });
   const right = connect(rightImpl, rightTransport, { foo: 33 });
@@ -149,47 +195,64 @@ test('can send events', done => {
 
 
 test('can send a promise that resolves later', async () => {
-  const [leftTransport, rightTransport] = createDuplex();
-  const left = connect(leftImpl, leftTransport, { foo: 42 });
-  const right = connect(rightImpl, rightTransport, { foo: 33 });
 
-  const { slow } = await left.slow11();
-  expect(slow).resolves.toStrictEqual(11);
+  const leftContract = emptyContract();
 
-  const { slow: slow2 } = await left.callMethod('slow11', []);
-  expect(slow2).resolves.toStrictEqual(11);
-});
-
-test('can send over TCP', async () => {
-  serveBunTCP({ impl: rightImpl, port: 2000, createState: () => ({ foo: 33 }) });
-  await sleep(100);
-  const socket = await Bun.connect<Transport>({
-    hostname: 'localhost',
-    port: 2000,
-    socket: {
-      async open(socket) {
-        const transport = {
-          input: new stream.Readable({ read() { } }),
-          output: new stream.Writable({
-            write(chunk, encoding, callback) {
-              socket.write(chunk);
-            },
-          }),
-          async close() {
-          }
-        }
-        const left = connect(
-          leftImpl,
-          transport,
-          { foo: 42 },
-        );
-        socket.data = transport;
-        expect(await left.callMethod('getLength', ["foobar"])).toStrictEqual(6);
-        expect(await left.callMethod('getState', [])).toStrictEqual(33);
-      },
-      data(socket, data) {
-        socket.data.input.push(data);
-      },
+  const rightContract = contract({
+    methods: {
+      slow11: t.callable([] as const, t.object({ slow: t.promise(t.number()) })),
     }
   });
+
+  const leftImpl = implement(leftContract, rightContract).finish();
+
+  const rightImpl = implement(rightContract, leftContract)
+    .method('slow11', () => {
+      return { slow: (async () => { await sleep(250); return 11; })() };
+    })
+    .finish();
+
+  const [leftTransport, rightTransport] = createDuplex();
+  const right = connect(leftImpl, leftTransport, { foo: 42 });
+  const left = connect(rightImpl, rightTransport, { foo: 33 });
+
+  const { slow: slow2 } = await right.callMethod('slow11', []);
+  expect(slow2).resolves.toStrictEqual(11);
+
+  const { slow } = await right.slow11();
+  expect(slow).resolves.toStrictEqual(11);
 });
+
+// test('can send over TCP', async () => {
+//   serveBunTCP({ impl: rightImpl, port: 2000, createState: () => ({ foo: 33 }) });
+//   await sleep(100);
+//   const socket = await Bun.connect<Transport>({
+//     hostname: 'localhost',
+//     port: 2000,
+//     socket: {
+//       async open(socket) {
+//         const transport = {
+//           input: new stream.Readable({ read() { } }),
+//           output: new stream.Writable({
+//             write(chunk, encoding, callback) {
+//               socket.write(chunk);
+//             },
+//           }),
+//           async close() {
+//           }
+//         }
+//         const left = connect(
+//           leftImpl,
+//           transport,
+//           { foo: 42 },
+//         );
+//         socket.data = transport;
+//         expect(await left.callMethod('getLength', ["foobar"])).toStrictEqual(6);
+//         expect(await left.callMethod('getState', [])).toStrictEqual(33);
+//       },
+//       data(socket, data) {
+//         socket.data.input.push(data);
+//       },
+//     }
+//   });
+// });

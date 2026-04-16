9d229199e2b31388b2175e8297d95ed3b3f3ecc8
Sam Vervaeck <samvv@pm.me>
2026-04-16 18:01:10 +0200

    Make `createProxy` return the correct type

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,9 +2,9 @@
 import type { RPC } from "./rpc.js";
 import type { ClientObj, Contract } from "./types.js";
 
-export function createProxy<L extends Contract, R extends Contract, S>(rpc: RPC<L, R, S>) {
-  // FIXME This `any` cast is a hack
-  return new Proxy<ClientObj<L, R, S>>(rpc as any, {
+export function createProxy<L extends Contract, R extends Contract, S>(rpc: RPC<L, R, S>): ClientObj<L, R, S> {
+  // FIXME This `any` cast looks like a hack
+  return new Proxy(rpc as any, {
     get(target, p, receiver) {
       if (typeof(p) === 'string') {
         if (rpc.impl.remote.hasMethod(p)) {

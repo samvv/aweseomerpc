8acf182fbcbb951590aa70bc80a404a739547877
Sam Vervaeck <samvv@pm.me>
2026-04-16 13:55:09 +0200

    Export WebSocketTransport in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export * from "./reflect.js"
-export { type Transport } from "./transport.js"
+export { type Transport, WebSocketTransport } from "./transport.js"
 export { implement, contract, emptyContract, anyContract } from "./types.js"
 export { RPC, type AnyMethods, type AnyEvents, connect } from "./rpc.js"
 export { createProxy } from "./proxy.js"

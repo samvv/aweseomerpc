74dcc6d86f7ebfaf0e5c217cf18a9c0c972882d3
Sam Vervaeck <samvv@pm.me>
2026-04-17 16:43:19 +0200

    Export more symbols from transport.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
 export * from "./reflect.js"
-export { type Transport, WebSocketTransport } from "./transport.js"
+export {
+  type Transport,
+  WebSocketTransport,
+  TransportError,
+  WebSocketError,
+  OpenWebSocketError,
+  GenericWebSocketError,
+  GenericOpenWebSocketError,
+  TimeoutReachedWebSocketError
+} from "./transport.js"
 export { type Client, implement, contract, emptyContract, anyContract } from "./types.js"
 export { RPC, type AnyMethods, type AnyEvents, connect } from "./rpc.js"
 export type { JSONValue, JSONArray, JSONObject } from "./util.js"
